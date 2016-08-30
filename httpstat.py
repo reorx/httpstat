125c28185689f26c7a2f7fa83feffc52c07c9c0e
reorx <novoreorx@gmail.com>
2016-08-30 14:29:17 +0800

    print header and body with style

--- httpstat.py
+++ httpstat.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 
+import os
 import json
 import sys
+import tempfile
 import subprocess
 
 
@@ -37,15 +39,42 @@ http_template = """
 """[1:]
 
 
-if __name__ == '__main__':
+def make_color(code):
+    def color_func(s):
+        tpl = '\x1b[{}m{}\x1b[0m'
+        return tpl.format(code, s)
+    return color_func
+
+
+red = make_color(31)
+green = make_color(32)
+yellow = make_color(33)
+blue = make_color(34)
+magenta = make_color(35)
+cyan = make_color(36)
+
+bold = make_color(1)
+underline = make_color(4)
+
+grayscale = {(i - 232): make_color('38;5;' + str(i)) for i in xrange(232, 256)}
+
+
+def main():
     url = sys.argv[1]
     if url.startswith('https://'):
         template = https_template
     else:
         template = http_template
 
+    # tempfile for output
+    bodyf = tempfile.NamedTemporaryFile(delete=False)
+    bodyf.close()
+
+    headerf = tempfile.NamedTemporaryFile(delete=False)
+    headerf.close()
+
     # run cmd
-    cmd = ['curl', '-w', curl_format, '-o', '/dev/null', '-s', url]
+    cmd = ['curl', '-w', curl_format, '-D', headerf.name, '-o', bodyf.name, '-s', url]
     output = subprocess.check_output(cmd)
 
     # parse output
@@ -62,14 +91,32 @@ if __name__ == '__main__':
         range_transfer=d['time_total'] - d['time_starttransfer'],
     )
 
+    # print header & body summary
+    with open(headerf.name, 'r') as f:
+        headers = f.read().strip()
+    # remote header file
+    os.remove(headerf.name)
+
+    for loop, line in enumerate(headers.split('\n')):
+        if loop == 0:
+            p1, p2 = tuple(line.split('/'))
+            print green(p1) + grayscale[14]('/') + cyan(p2)
+        else:
+            pos = line.find(':')
+            print grayscale[14](line[:pos + 1]) + cyan(line[pos + 1:])
+
+    print
+    print '{} stored in: {}'.format(green('Body'), bodyf.name)
+
+    # print stat
+
     def fmta(s):
-        return '{:>4}ms'.format(s)
+        return cyan('{:>4}ms'.format(s))
 
     def fmtb(s):
-        return '{:<6}'.format(str(s) + 'ms')
+        return cyan('{:<6}'.format(str(s) + 'ms'))
 
-    # render result
-    rs = template.format(
+    stat = template.format(
         # a
         a000=fmta(d['range_dns']),
         a001=fmta(d['range_connection']),
@@ -83,4 +130,9 @@ if __name__ == '__main__':
         b003=fmtb(d['time_starttransfer']),
         b004=fmtb(d['time_total']),
     )
-    print rs
+    print
+    print stat
+
+
+if __name__ == '__main__':
+    main()
