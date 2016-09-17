c41f80855c0d7d5b7e9419adbeb2edb2c1094abd
tobyvessalius <tobyvessalius@hotmail.com>
2016-09-17 13:56:18 -0400

    change the color of http&body to red

--- httpstat.py
+++ httpstat.py
@@ -73,11 +73,11 @@ def make_color(code):
 
 
 red = make_color(30)
-green = make_color(37)
-yellow = make_color(35)
-blue = make_color(36)
-magenta = make_color(53)
-cyan = make_color(24)
+green = make_color(31)
+yellow = make_color(33)
+blue = make_color(34)
+magenta = make_color(35)
+cyan = make_color(36)
 
 bold = make_color(1)
 underline = make_color(4)
