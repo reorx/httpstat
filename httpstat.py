d0ebcdab81d4af1edc061c18416c9a7b29fd16be
reorx <novoreorx@gmail.com>
2016-09-27 17:44:04 +0800

    complete help text of env vars

--- httpstat.py
+++ httpstat.py
@@ -121,10 +121,16 @@ Options:
   --version     show version.
 
 Environments:
-  HTTPSTAT_SHOW_BODY    By default httpstat will write response body
-                        in a tempfile, but you can let it print out by setting
-                        this variable to `true`.
-  HTTPSTAT_SHOW_SPEED   set to `true` to show download and upload speed.
+  HTTPSTAT_SHOW_BODY    Set to `true` to show resposne body in the output,
+                        note that body length is limited to 1023 bytes, will be
+                        truncated if exceeds. Default is `false`.
+  HTTPSTAT_SHOW_SPEED   Set to `true` to show download and upload speed.
+                        Default is `false`.
+  HTTPSTAT_SAVE_BODY    By default httpstat stores body in a tmp file,
+                        set to `false` to disable this feature. Default is `true`
+  HTTPSTAT_CURL_BIN     Indicate the curl bin path to use. Default is `curl`
+                        from current shell $PATH.
+  HTTPSTAT_DEBUG        Set to `true` to see debugging logs. Default is `false`
 """[1:-1]
     print(help)
 
